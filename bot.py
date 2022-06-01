dcf647d2ca6bfdaa82d0cf153df0e98a57f3e3ad
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2022-06-01 15:58:43 +0300

    try to use webhooks

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
+import logging
+import os
+
 from aiogram import executor
+
+import config
 from utils.set_bot_commands import set_default_commands
 from handlers.users import dp
 from loader import bot, storage
@@ -6,12 +11,21 @@ from loader import bot, storage
 
 async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
+    await bot.set_webhook(config.WEBHOOK_URL, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
-    await bot.close()
-    await storage.close()
+    await bot.delete_webhook()
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown, skip_updates=False)
+    logging.basicConfig(level=logging.INFO)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=config.WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=config.WEBAPP_HOST,
+        port=config.WEBAPP_PORT,
+)
--- config.py
+++ config.py
@@ -4,3 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
